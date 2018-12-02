3ce6ed7da191b81bb81aa259aad029a1526028ac
mackah666 <mackah666@yahoo.co.uk>
2018-12-02 11:56:59 +0000

    Implement feature to pass first test

--- features/step_definitions/addition_steps.js
+++ features/step_definitions/addition_steps.js
@@ -4,9 +4,11 @@ const Calculator = require('../../lib/calculator');
 
 let calculator;
 
-Given('the numbers {int} and {int}', function (int, int2, callback) {
+Given('the numbers {int} and {int}', function (x, y, callback) {
     // Write code here that turns the phrase above into concrete actions
-    callback(null, 'pending');
+    calculator = new Calculator(x,y);
+    callback();
+	
 });
 
 
