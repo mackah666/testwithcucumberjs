916c508c1ab81f8755c961286cea4a01092801d4
michael ackah <mackah666@yahoo.co.uk>
2019-01-18 14:47:31 +0000

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -5,9 +5,7 @@ pipeline {
 		stage('Run Cucumber') {
 			steps{
     				// some block
-				sh 'ls -al'
-				sh "/usr/local/bin/npm install"
-				//sh '/usr/local/bin/node -v'			
+				sh 'npm install'			
 			}	
 		}
 	}
